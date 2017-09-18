49912b0c89e382a18bd9d53d1490ec0f6b27fd1a
alex <iseldomused@gmail.com>
2017-09-19 02:11:51 +0300

    task 1

---
+++ 02. vars/01_product/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Tutorial
+{
+    public static void Main()
+    {
+        // пишите свой код здесь
+
+        // тестовый код
+        Console.WriteLine("productName: " + productName);
+        Console.WriteLine("productYear: " + productYear);
+        Console.WriteLine("productPrice: " + productPrice);
+
+        Console.ReadLine();
+    }
+}
