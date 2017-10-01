b30dcdd73f9ba2cb79a7d00429483bae6f074cad
maxcore25 <mkorobov2002@mail.ru>
2017-10-01 16:16:44 +0300

    Конвертер расстояний

---
+++ 03. ifs/Converts/Converts/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input distance in cm");
+            double cms = int.Parse(Console.ReadLine());
+            double meters = (double)cms / 100;
+            Console.WriteLine("metres =" + meters);
+            Console.ReadLine();
+
+        }
+    }
+}
