15a4894d6155e9c5dfbfed35d9772d5814f20b11
maxcore25 <mkorobov2002@mail.ru>
2017-10-01 16:27:47 +0300

    Винрейт

---
+++ 03. ifs/Winrate/Winrate/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравстуйте! Сколько игр Вы успели сыграть?");
+            int games = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сколько игр Вы выиграли?");
+            int wongames = int.Parse(Console.ReadLine());
+            double winrate = (double)wongames / games;
+            Console.WriteLine("Ваш винрейт составляет: " + winrate);
+            Console.ReadLine();
+
+
+        }
+    }
+}
