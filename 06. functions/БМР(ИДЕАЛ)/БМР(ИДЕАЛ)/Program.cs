54820cc714448075a68adcfe605430c4d7625431
maxcore25 <mkorobov2002@mail.ru>
2017-10-29 15:28:55 +0300

    БМР(ИДЕАЛ)

---
+++ 06. functions/БМР(ИДЕАЛ)/БМР(ИДЕАЛ)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Введите первое целое число:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе целое число:");
+            int b = int.Parse(Console.ReadLine());
+            comparison(a, b);
+        }
+        static void comparison(int a, int b)
+        {
+            if (a > b)
+            {
+                Console.WriteLine(">");
+            }
+            else if (a == b)
+            {
+                Console.WriteLine("=");
+            }
+            else if (a < b)
+            {
+                Console.WriteLine("<");
+            }
+            Console.ReadLine();
+        }
+    }
+}
