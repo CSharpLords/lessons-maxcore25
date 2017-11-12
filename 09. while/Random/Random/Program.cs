14d204c72dcea83c8ebecb148ee620f7d13c3beb
maxcore25 <mkorobov2002@mail.ru>
2017-11-12 17:05:03 +0300

    Random

---
+++ 09. while/Random/Random/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            while (true)
+            {
+                int number = rand.Next(0, 100);
+                Console.WriteLine("запомните число: " + number);
+                Thread.Sleep(2000);
+                Console.Clear();
+                Console.WriteLine("введите запомнинное число");
+                int guess = int.Parse(Console.ReadLine());
+                if (guess == number)
+                {
+                    Console.WriteLine("ага");
+                }
+                else
+                {
+                    Console.WriteLine("неа");
+                }
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+            Console.ReadLine();
+        }
+    }
+}
