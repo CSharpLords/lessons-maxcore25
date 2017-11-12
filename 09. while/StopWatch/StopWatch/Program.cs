abee13b12f88f47bfc0577c0cbff33db772898db
maxcore25 <mkorobov2002@mail.ru>
2017-11-12 16:27:23 +0300

    Пароль

---
+++ 09. while/StopWatch/StopWatch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sec = 0;
+            while (true)
+            {
+                sec = sec + 1;
+                Console.WriteLine(sec);
+                Thread.Sleep(1);
+            }
+        }
+    }
+}
