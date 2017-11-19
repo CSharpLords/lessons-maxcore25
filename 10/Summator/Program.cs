548e29b27e6ee97dbc1e34f825a8057383f6da17
maxcore25 <mkorobov2002@mail.ru>
2017-11-19 16:14:13 +0300

    summator

--- 10/Summator/Program.cs
+++ 10/Summator/Program.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Summator {
-    class Program {
-        static void Main(string[] args) {
-            while (true) {
-                int sum = 0;
-                Console.WriteLine("Введите число");
-
+namespace Summator
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int sum = 0;
+            while (true) 
+            {   
+                Console.WriteLine("Введите число:");
                 int number = int.Parse(Console.ReadLine());
-                Console.WriteLine("Сумма всех чисел: " + (sum + number));
+                sum = sum + number;
+                Console.WriteLine("Сумма всех чисел: " + sum);
                 Console.WriteLine();
             }
         }
