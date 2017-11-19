ec6b741886e76ad8a5a522d17f9f7f5a817590f9
Simplifier <iseldomused@gmail.com>
2017-11-19 15:03:46 +0300

    add 10

---
+++ 10/Summator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Summator {
+    class Program {
+        static void Main(string[] args) {
+            while (true) {
+                int sum = 0;
+                Console.WriteLine("Введите число");
+
+                int number = int.Parse(Console.ReadLine());
+                Console.WriteLine("Сумма всех чисел: " + (sum + number));
+                Console.WriteLine();
+            }
+        }
+    }
+}
