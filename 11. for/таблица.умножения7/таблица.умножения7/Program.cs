27ec08c804ae8fcec32706d7706e78f938995146
maxcore25 <mkorobov2002@mail.ru>
2017-12-03 16:04:59 +0300

    таблица умножения на 7

---
+++ 11. for/таблица.умножения7/таблица.умножения7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int anotherNumber = 1; anotherNumber < 10; anotherNumber = anotherNumber + 1)
+            {
+                int multiplication = anotherNumber * 7;
+                Console.WriteLine(anotherNumber + " * 7 = " + multiplication);
+
+                Thread.Sleep(1000);
+            }
+            Console.ReadLine();
+        }
+    }
+}
