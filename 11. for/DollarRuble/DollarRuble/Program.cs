7cc74a3b4534c3415848e6348ad5fb3f11d0633a
maxcore25 <mkorobov2002@mail.ru>
2017-12-03 16:52:46 +0300

    DollarRuble

---
+++ 11. for/DollarRuble/DollarRuble/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int dollar = 0; dollar < 21; dollar = dollar + 1)
+            {
+                int ruble = dollar * 60;
+                Console.WriteLine(dollar + " dollars = " + ruble + " rubles.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
