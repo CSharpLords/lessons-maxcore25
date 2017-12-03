797e22db118d2a2b2caccebabb48e84a37f244c2
maxcore25 <mkorobov2002@mail.ru>
2017-12-03 16:52:08 +0300

    10.10.4

---
+++ 11. for/10.10.4/10.10.4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 10; number < 26; number = number + 1)
+            {
+                double a = .4;
+                Console.WriteLine(number + " " + (number + a));
+            }
+            Console.ReadLine();
+        }
+    }
+}
