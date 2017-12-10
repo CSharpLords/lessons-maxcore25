baaf4da339c2eafc209424cb24f7c9ef817d6fe6
maxcore25 <mkorobov2002@mail.ru>
2017-12-10 15:42:51 +0300

    Контрольная Работа №1(Василий)

---
+++ 13.Контроха/КонтрольнаяРабота(13)/КонтрольнаяРабота(13)/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace КонтрольнаяРабота_13_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет! У тебя есть компуктер?");
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "нет")
+            {
+                Console.WriteLine("Ну ок, тогда нам не о чем говорить. Пока!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (answer == "да")
+            {
+                Console.WriteLine("Сколько часов в неделю ты проводишь за ним?");
+            }
+            int time = int.Parse(Console.ReadLine());
+            if (time < 14)
+            {
+                Console.WriteLine("А большую часть этого времени ты работаешь или отдыхаешь?");
+                string answer1 = Console.ReadLine().ToLower();
+                if (answer1 == "работаю")
+                {
+                    Console.WriteLine("Ты и отдохнуть не забывай!");
+                }
+                else if (answer1 == "отдыхаю")
+                {
+                    Console.WriteLine("Хорошо, что ты знаешь меру");
+                }
+            }
+            else if (time > 14)
+            {
+                Console.WriteLine("А большую часть этого времени ты работаешь или отдыхаешь?");
+                string answer2 = Console.ReadLine().ToLower();
+                if (answer2 == "работаю")
+                {
+                    Console.WriteLine("Пожалей себя!!");
+                }
+                else if (answer2 == "отдыхаю")
+                {
+                    Console.WriteLine("Ты портишь свое здоровье таким поведением!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
