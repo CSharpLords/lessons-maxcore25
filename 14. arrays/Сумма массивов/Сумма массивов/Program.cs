57555cd1b68bde772e2bc8b17670ea2a0c34d89d
maxcore25 <mkorobov2002@mail.ru>
2017-12-17 16:59:20 +0300

    Сумма массивов

---
+++ 14. arrays/Сумма массивов/Сумма массивов/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massive = { 12, 24, 44, 50, 100 };
+            int sum = 0;
+            for (int number = 0; number < massive.Length; number++)
+            {
+                sum = sum + massive[number];
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
