5d88418a2d82435ef133879d131901fe6ff31646
maxcore25 <mkorobov2002@mail.ru>
2017-12-24 16:44:05 +0300

    чётные числа

---
+++ 15. arrays/чётные числа/чётные числа/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 2, 4, 5, 6, 7, 8, 10};
+            for (int num = 0; num < array.Length; num++)
+            {
+                double result = array[num] % 2;
+                if (result == 0)
+                {
+                    Console.WriteLine(array[num]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
