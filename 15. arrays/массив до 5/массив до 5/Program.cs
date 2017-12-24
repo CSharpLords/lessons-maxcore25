41d7dc8297c4ec465df57086683e9c2fef281984
maxcore25 <mkorobov2002@mail.ru>
2017-12-24 15:26:40 +0300

    массив до 5

---
+++ 15. arrays/массив до 5/массив до 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massive = {1, 2, 3, 3, 5, 7, 5, 3, 5};
+            int sum = 0;
+            for (int number = 0; number < massive.Length; number++)
+            {
+                if (massive[number] == 5)
+                {
+                    Console.WriteLine("Нашлось число в массиве с числом 5: " + number);
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
