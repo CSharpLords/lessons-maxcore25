d8ee43a8b59926f2f0ec2f12d46c240fd664c093
maxcore25 <mkorobov2002@mail.ru>
2017-12-24 16:44:25 +0300

    числа кратные 10

---
+++ 15. arrays/кратные 10/кратные 10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {1, 10, 30, 3, 2, 20};
+            for (int num = 0; num < array.Length; num++)
+            {
+                double result = array[num] % 10;
+                if (result == 0)
+                {
+                    Console.WriteLine(array[num]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
