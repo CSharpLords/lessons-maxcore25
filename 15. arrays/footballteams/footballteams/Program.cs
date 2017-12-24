7eb6cf074da247ca26840b2100c9864229b83580
maxcore25 <mkorobov2002@mail.ru>
2017-12-24 15:31:35 +0300

    footballteams

---
+++ 15. arrays/footballteams/footballteams/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] massive = { 1, 2, 4, 5, 6, 7, 8, 9, 0 };
+            for (int number = 0; number < massive.Length; number++)
+            {
+                if (massive[number] < 3)
+                {
+                    Console.WriteLine("У команды: " + number + " меньше 3 побед");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
