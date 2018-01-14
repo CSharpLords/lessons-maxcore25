d959fddf279dad6412b1b98864db2eac8de3fb76
maxcore25 <mkorobov2002@mail.ru>
2018-01-14 17:08:57 +0300

    Первый проект Unity

---
+++ 16/Number1/Assets/C#Script/TestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TestScript : MonoBehaviour 
+{
+	float time = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		print ("Координаты: " + "x = "+ transform.position.x + ", y = " + transform.position.y + ", z = " + transform.position.z);
+		time = time + Time.deltaTime;
+		int seconds = (int)time;
+		print ("Количество секунд: " + seconds);
+	}
+}
