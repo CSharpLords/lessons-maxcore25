c4aebabc21eb1f96bec6ddc3f3850b2be6d53d58
maxcore25 <mkorobov2002@mail.ru>
2018-01-28 17:13:03 +0300

    Plane (or Enemy)

---
+++ 17. unity. conditions/Enemy/Assets/C#Scripts/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour 
+{
+	public Transform enemy;
+	// Use this for initialization
+	void Start () 
+	{
+		print (enemy.position.x);
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		float dist = Vector3.Distance(enemy.position, transform.position);
+		print (dist);
+	}
+}
---
+++ 17. unity. conditions/plane/Assets/C#Script/Maxhight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maxhight : MonoBehaviour 
+{
+	float maxHight = 0;
+	float minHight = 0;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (transform.position.y > maxHight) 
+		{
+			maxHight = transform.position.y;
+			print ("Максимальная высота: " + maxHight);
+		}
+		if (transform.position.y < minHight) 
+		{
+			minHight = transform.position.y;
+			print ("Минимальная высота: " + minHight);
+		}
+	}
+}
