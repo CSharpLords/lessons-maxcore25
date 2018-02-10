0589f8aec93ce04115f7f70b5e8fcb3b025f72b0
maxcore25 <mkorobov2002@mail.ru>
2018-02-11 01:22:07 +0300

    18

--- 18/teleportation/Assets/C#Script/Timer.cs
+++ 18/teleportation/Assets/C#Script/Timer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Timer : MonoBehaviour 
 {
 	public Transform enemy;
-	float time = 0;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -15,13 +15,7 @@ public class Timer : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{
-		time = time + Time.deltaTime;
-		int seconds = (int)time;
-		//print(seconds);
-		if (seconds == 15) 
-		{
-			transform.position = new Vector3 (0, 0, 0); 
-		}
+		
 		float dist = Vector3.Distance(enemy.position, transform.position);
 		if (dist <= 3) 
 		{
