46c6f5d2362efa8780afe8432c3e8c697b3ac925
maxcore25 <mkorobov2002@mail.ru>
2018-02-25 16:45:54 +0300

    Game16+

---
+++ 21/Game16+/Assets/C#Scripts/MinusAge.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinusAge : MonoBehaviour 
+{
+
+	public Text ageField;
+	public static int age = 18;
+
+	void Start () 
+	{
+		
+		Button plus = GetComponent<Button> ();
+		plus.onClick.AddListener (Decrease);
+		ageField.text = "18";
+
+
+	}
+	
+	// Update is called once per frame
+	void  Decrease () 
+	{
+		age = age - 1;
+		ageField.text = age.ToString ();
+	}
+}
---
+++ 21/Game16+/Assets/C#Scripts/PlusAge.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlusAge : MonoBehaviour 
+{
+	
+	public Text ageField;
+	// Use this for initialization
+	void Start () 
+	{
+		
+		Button plus = GetComponent<Button> ();
+		plus.onClick.AddListener (Increase);
+		ageField.text = "18";
+	}
+	
+	// Update is called once per frame
+	void Increase () 
+	{
+		MinusAge.age = MinusAge.age + 1;
+		ageField.text = MinusAge.age.ToString ();
+
+	}
+}
