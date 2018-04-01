c0ea0e35a0f987ac6dba36365669446c4d5d6926
maxcore25 <mkorobov2002@mail.ru>
2018-04-01 16:58:16 +0300

    CalcPower

---
+++ 26/CalcPower/CalcPower/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CalcPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int firstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Введите степень: ");
+            int secondNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(firstNumber + "^" + secondNumber + "=" + Power(firstNumber, secondNumber));
+            Console.ReadLine();
+        }
+
+        static int Power(int number, int exponent)
+        {
+            int result = number;
+            for (int n = 1; n < exponent; n++ )
+            {
+                result = result * number;
+            }
+            
+            return result;
+        }
+    }
+}
