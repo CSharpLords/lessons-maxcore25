39bacb8514d874b6a67d3cef0097ff7c4fe88531
maxcore25 <mkorobov2002@mail.ru>
2018-04-08 19:07:17 +0300

    Mathematics

--- 25. methods/Mathematics/Program.cs
+++ 25. methods/Mathematics/Program.cs
@@ -1,10 +1,24 @@
 ﻿using System;
 
-namespace Mathematics {
-	class Program {
-		static void Main(string[] args) {
+namespace Mathematics 
+{
+	class Program 
+    {
+		static void Main(string[] args) 
+        {
 			Console.WriteLine(Sum(3, Multiply(7, 9)));
 			Console.ReadLine();
 		}
+        static int Sum(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            return sum;
+        }
+        static int Multiply(int firstNumber, int secondNumber)
+        {
+            int multiply = firstNumber * secondNumber;
+            return multiply;
+        }
+
 	}
 }
