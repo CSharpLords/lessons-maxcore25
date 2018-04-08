e84d866fccdbf742d40bd0d759c2fa5761f868f5
maxcore25 <mkorobov2002@mail.ru>
2018-04-08 16:58:10 +0300

    UpgradedSpaces

--- 25. methods/UpgradedSpaces/Program.cs
+++ 25. methods/UpgradedSpaces/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 
-namespace UpgradedSpaces {
-	class Program {
-		static void Main(string[] args) {
+namespace UpgradedSpaces 
+{
+	class Program 
+    {
+		static void Main(string[] args)
+        {
 			Console.Write("Введите предложение: ");
 			string sentence = Console.ReadLine();
 
@@ -11,5 +14,33 @@ namespace UpgradedSpaces {
 			Console.Write("Предложение '" + sentence + "' содержит " + spacesAmount + " " + spacesForm);
 			Console.ReadLine();
 		}
+        static int CountSpaces(string sentence)
+        {
+            int space = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] == ' ')
+                {
+                    space++;
+                }
+            }
+            return space;
+        }
+        static string GetSpacesForm(int spacesAmount)
+        {
+            if(spacesAmount == 1)
+            {
+                return "пробел";
+            }
+            if (spacesAmount > 1 && spacesAmount < 5)
+            {
+                return "пробелa";
+            }
+            if (spacesAmount > 4 && spacesAmount < 21)
+            {
+                return "пробелов";
+            }
+            return "";
+        }
 	}
 }
