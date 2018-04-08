54b72ed5a2bccb71a9167288afe612ec4659d643
maxcore25 <mkorobov2002@mail.ru>
2018-04-08 16:37:34 +0300

    Spaces

--- 25. methods/Spaces/Program.cs
+++ 25. methods/Spaces/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 
-namespace Spaces {
-	class Program {
-		static void Main(string[] args) {
+namespace Spaces 
+{
+	class Program 
+    {
+		static void Main(string[] args) 
+        {
 			Console.Write("Введите предложение: ");
 			string sentence = Console.ReadLine();
 
@@ -10,5 +13,19 @@ namespace Spaces {
 			Console.Write("Предложение '" + sentence + "' содержит " + spacesAmount + " пробелов");
 			Console.ReadLine();
 		}
+
+        static int CountSpaces(string sentence)
+        {
+            int space = 0;
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if(sentence[i] == ' ')
+                {
+                    space++;
+                }
+            }
+            return space;
+        }
+        
 	}
 }
