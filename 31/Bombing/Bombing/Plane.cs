23cfa472e95e3008e96a31dca164c6b11f50d7c3
maxcore25 <mkorobov2002@mail.ru>
2018-05-13 17:02:26 +0300

    Bombing

---
+++ 31/Bombing/Bombing/Plane.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bombing
+{
+    class Plane
+    {
+        public string plane = ">";
+        private int speed = 5;
+        public int x = 0;
+        public int y = 0;
+        public void Flight()
+        {
+            for (int i = 0; i < 100; i += speed)
+            {
+                Console.Write(plane);
+            }
+        }
+    }
+}
---
+++ 31/Bombing/Bombing/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Bombing
+{
+    class Program
+    {
+        static Plane plane = new Plane();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                // Console.KeyAvailable становится равен тру, когда мы нажали любую клавишу.
+                // так что повторяем, пока кнопка не нажата
+                while (!Console.KeyAvailable)
+                {
+                    Console.Clear();
+                    Update(); // выполняем основные действия
+                    Thread.Sleep(100);
+                }
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+               
+                if (keyInfo.Key == ConsoleKey.Spacebar)
+                {
+                    
+                }
+            }
+
+           
+            
+            Console.ReadLine();
+        }
+        static void Update()
+        {
+            
+            plane.Flight();
+        }
+    }
+}
