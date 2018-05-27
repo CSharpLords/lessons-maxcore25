e847650db22f2d4ea0d5e88e8da21921a345c916
maxcore25 <mkorobov2002@mail.ru>
2018-05-27 17:03:24 +0300

    Tanks

---
+++ 31/Tanks/Tanks/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+
+namespace Tanks
+{
+    //https://github.com/CSharpWizards/homework_w-pchuck77/tree/master/13
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                while (!Console.KeyAvailable)
+                {
+                    Console.Clear();
+                    Update(); 
+                    Thread.Sleep(100);
+                }
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                if (keyInfo.Key == ConsoleKey.Spacebar)
+                {
+                    
+                }
+            }
+        }
+        static void Update()
+        {
+
+        }
+    }
+}
---
+++ 31/Tanks/Tanks/Tank.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tanks
+{
+    class Tank
+    {
+        public string tank = "0";
+        public int x;
+        public int y;
+        public void DrawTank()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(tank + "|");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        
+    }
+}
