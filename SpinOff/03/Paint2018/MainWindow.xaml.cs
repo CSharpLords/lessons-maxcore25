50c4d22de5bd7f868f3c6de8db5dbd872bcce176
maxcore25 <mkorobov2002@mail.ru>
2018-10-06 00:27:03 +0300

    Paint2018

--- SpinOff/03/Paint2018/MainWindow.xaml.cs
+++ SpinOff/03/Paint2018/MainWindow.xaml.cs
@@ -21,6 +21,9 @@ namespace Paint2018
     public partial class MainWindow : Window
     {
         Point currentPoint;
+        Point eraserCurrentPoint;
+        static bool switchOnEraser = false;
+        static byte clicksAmount = 0;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,9 +31,9 @@ namespace Paint2018
 
         private void Canvas_MouseMove(object sender, MouseEventArgs e)
         {
-            
-            if (e.LeftButton == MouseButtonState.Pressed)
+            if (e.LeftButton == MouseButtonState.Pressed && switchOnEraser == false)
             {
+                Mouse.OverrideCursor = Cursors.Pen;
                 Line line = new Line();
                 line.Stroke = new SolidColorBrush(ColorPicker.SelectedColor ?? Brushes.Black.Color);
                 line.StrokeThickness = 1;
@@ -43,32 +46,57 @@ namespace Paint2018
 
                 Canvas.Children.Add(line);
             }
+            if (switchOnEraser == true && e.LeftButton == MouseButtonState.Pressed)
+            {
+                Mouse.OverrideCursor = Cursors.Cross;
+                Line line = new Line();
+                line.Stroke = new SolidColorBrush(Brushes.LightGray.Color);
+                line.StrokeThickness = 5;
+                line.X1 = eraserCurrentPoint.X;
+                line.Y1 = eraserCurrentPoint.Y;
+                line.X2 = e.GetPosition(Canvas).X;
+                line.Y2 = e.GetPosition(Canvas).Y;
 
+                eraserCurrentPoint = e.GetPosition(Canvas);
+
+                Canvas.Children.Add(line);
+            }
         }
 
         private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            currentPoint = e.GetPosition(Canvas);
+            if (switchOnEraser == true)
+            {
+                eraserCurrentPoint = e.GetPosition(Canvas);
+            }
+            else
+            {
+                currentPoint = e.GetPosition(Canvas);
+            }
         }
 
         private void Eraser_Click(object sender, RoutedEventArgs e)
         {
-            Line line = new Line();
-            line.Stroke = new SolidColorBrush(ColorPicker.SelectedColor ?? Brushes.Black.Color);
-            line.StrokeThickness = 1;
-            line.X1 = currentPoint.X;
-            line.Y1 = currentPoint.Y;
-            line.X2 = e.GetPosition(Canvas).X;
-            line.Y2 = e.GetPosition(Canvas).Y;
+            clicksAmount++;
+            if (clicksAmount == 2)
+            {
+                switchOnEraser = false;
+                clicksAmount = 0;
+                Eraser.Content = "Eraser";
+                Mouse.OverrideCursor = Cursors.Pen;
+            }
+            else
+            {
+                switchOnEraser = true;
+                Eraser.Content = "Brush";
+                Mouse.OverrideCursor = Cursors.Cross;
+            }
 
-            currentPoint = e.GetPosition(Canvas);
-
-            Canvas.Children.Add(line);
         }
 
         private void BrushSize_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-
+            
         }
     }
 }
