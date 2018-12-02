3f5585b682603e465fa489246db658a6a1dd4682
maxcore25 <mkorobov2002@mail.ru>
2018-12-02 23:32:23 +0300

    AsianCuisine

---
+++ SpinOff/04/AsianCuisine/IconButton.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace AsianCuisine
+{
+    /// <summary>
+    /// Логика взаимодействия для IconButton.xaml
+    /// </summary>
+    public partial class IconButton : Button
+    {
+        public static DependencyProperty IconProperty =
+            DependencyProperty.Register(
+                "Icon", typeof(string), typeof(IconButton)
+            );
+        
+        [Category("Common")]
+        public string Icon
+        {
+            get { return (string)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static DependencyProperty LabelProperty =
+            DependencyProperty.Register(
+                "Label", typeof(string), typeof(IconButton)
+            );
+
+        [Category("Common")]
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+
+        public IconButton()
+        {
+            InitializeComponent();
+        }
+
+        private void iconButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService nav;
+            nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new Uri("DishTypes.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
