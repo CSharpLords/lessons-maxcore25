ffa7014b373475ac4049d025cdd96a7905ef3603
maxcore25 <mkorobov2002@mail.ru>
2018-12-09 19:27:53 +0300

    AsianCuisine

--- SpinOff/04/AsianCuisine/MainWindow.xaml.cs
+++ SpinOff/04/AsianCuisine/MainWindow.xaml.cs
@@ -10,6 +10,30 @@ namespace AsianCuisine
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+        bool chinaIsClicked;
+        bool japanIsClicked;
+        bool southKoreaIsClicked;
+        private void China_Click(object sender, RoutedEventArgs e)
+        {
+            chinaIsClicked = true;
+            japanIsClicked = false;
+            southKoreaIsClicked = false;
+        }
+
+        private void Japan_Click(object sender, RoutedEventArgs e)
+        {
+            japanIsClicked = true;
+            chinaIsClicked = false;
+            southKoreaIsClicked = false;
+        }
+
+        private void SouthKorea_Click(object sender, RoutedEventArgs e)
+        {
+            southKoreaIsClicked = true;
+            chinaIsClicked = false;
+            japanIsClicked = false;
         }
     }
 }
